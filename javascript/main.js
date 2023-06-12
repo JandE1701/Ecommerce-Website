a51df5a74d6e0b34c9edfeba3de567250969139c
Juan Carlos Bohórquez Guerra <jucabogue@hotmail.com>
2023-06-12 18:41:04 -0500

    Se creo la parte del menu mobile y desktop

---
+++ javascript/main.js
@@ -0,0 +1,70 @@
+/* Copy menu for mobile */
+
+function copyMenu() {
+    // Copiar el contenido de .dpt-cat a .departments
+    const dptCategory = document.querySelector(".dpt-cat");
+    const dptPlace = document.querySelector(".departments");
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    // Copiar el contenido de .header-nav nav a .off-canvas nav
+    const mainNav = document.querySelector(".header-nav nav");
+    const navPlace = document.querySelector(".off-canvas nav");
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    // Clonar .header-top .wrapper y añadirlo a .thetop-nav en .off-canvas
+    const topNav = document.querySelector(".header-top .wrapper");
+    const topPlace = document.querySelector(".off-canvas .thetop-nav");
+
+    /* Al clonar el contenido de .header-top .wrapper, hemos utilizado el método cloneNode(true) para clonar todo el contenido y luego añadirlo a .thetop-nav en .off-canvas. */
+    const clonedNav = topNav.cloneNode(true);
+
+    /* Antes de añadir el contenido clonado, hemos eliminado cualquier contenido existente en .thetop-nav utilizando topPlace.innerHTML = ''. */
+    topPlace.innerHTML = "";
+    topPlace.appendChild(clonedNav);
+}
+
+copyMenu();
+
+/* Mostramos el menu en vistas mobile */
+const menuButton = document.querySelector(".trigger");
+const closeButton = document.querySelector(".t-close");
+const site = document.querySelector(".site");
+
+menuButton.addEventListener("click", function () {
+    site.classList.toggle("showmenu");
+});
+
+closeButton.addEventListener("click", function () {
+    site.classList.remove("showmenu");
+});
+
+
+/* Mostramos el sub menu en vistas mobile */
+
+/* Se utiliza el nombre plural submenus para representar la colección de elementos. */
+const submenus = document.querySelectorAll(".has-child .icon-small");
+
+/* Se utiliza una función separada toggleSubMenu para manejar el evento de clic y alternar la clase expand en el elemento padre */
+function toggleSubMenu(e) {
+    e.preventDefault();
+
+    /* Se utiliza const en lugar de let para declarar las constantes 'parent' y 'expandClass' ya que no se reasignan. */
+    const parent = this.closest(".has-child");
+    const expandClass = "expand";
+
+    /* Se utiliza un ciclo forEach para recorrer todos los submenús y se remueve la clase expand de los elementos que no son el padre del submenú actual. */
+    submenus.forEach((submenu) => {
+        const submenuParent = submenu.closest(".has-child");
+        /* Se utiliza una comprobación submenuParent !== parent para asegurarse de que solo se remueva la clase expand de los elementos que no son el padre del submenú actual. */
+        if (submenuParent !== parent) {
+            submenuParent.classList.remove(expandClass);
+        }
+    });
+
+    /* Se utiliza parent.classList.toggle(expandClass) para alternar la clase expand en el elemento padre. */
+    parent.classList.toggle(expandClass);
+}
+
+submenus.forEach((submenu) => {
+    submenu.addEventListener("click", toggleSubMenu);
+});
